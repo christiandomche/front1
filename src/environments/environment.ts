7f2f1fca50fd7137f270196cf76cb84abe3ecdb5
christiandomche <107481952+christiandomche@users.noreply.github.com>
2024-09-03 16:17:22 +0100

    First Push

---
+++ src/environments/environment.ts
@@ -0,0 +1,37 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:8000/api',
+  apiBaseUrlUpload: 'http://localhost:8000/'
+
+  // apiBaseUrl: 'https://v0.smartschool.school/api',
+  // apiBaseUrlUpload: 'https://v0.smartschool.school/'
+
+  // apiBaseUrl: 'https://rn.smartschool.school/api',
+  // apiBaseUrlUpload: 'https://rn.smartschool.school/'
+
+  // apiBaseUrl: 'https://jd.smartschool.school/api',
+  // apiBaseUrlUpload: 'https://jd.smartschool.school/'
+
+  // apiBaseUrl: 'https://back.jbnpssoft.com/api',
+  // apiBaseUrlUpload: 'https://back.jbnpssoft.com/'
+
+  // apiBaseUrl: 'https://back.bertinosoft.com/api',
+  // apiBaseUrlUpload: 'https://back.bertinosoft.com/'
+
+  // apiBaseUrl: 'https://back.renaissancesoft.com/api',
+  // apiBaseUrlUpload: 'https://back.renaissancesoft.com/'
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
